2d64eee42b02830a592c1872295855b5c9ba0cf8
AntonioCS <antoniocs@gmail.com>
2019-07-25 16:35:17 +0100

    Added logic to process given file

--- src/Command/AppAnalyseMetricsCommand.php
+++ src/Command/AppAnalyseMetricsCommand.php
@@ -2,10 +2,14 @@
 
 namespace App\Command;
 
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use App\Service\DataAnalyzer\DataAnalyzer;
+use App\Service\DataAnalyzer\Result;
+use App\Service\DataAnalyzer\Formatters\TextFormatter;
 
 /**
  * Class AppAnalyseMetricsCommand
@@ -18,6 +22,25 @@ class AppAnalyseMetricsCommand extends Command
      * @var string
      */
     protected static $defaultName = 'app:analyse-metrics';
+
+    /**
+     * @var DataAnalyzer
+     */
+    private $dataAnalyzer;
+
+    /**
+     * @var TextFormatter;
+     */
+    private $textFormatter;
+
+    public function __construct(DataAnalyzer $dataAnalyzer, TextFormatter $textFormatter)
+    {
+        $this->dataAnalyzer = $dataAnalyzer;
+        $this->textFormatter = $textFormatter;
+
+        parent::__construct();
+    }
+
 
     /**
      * Configure the command.
@@ -36,6 +59,17 @@ class AppAnalyseMetricsCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
-        // Your business logic goes here.
+        $metricsFile = $input->getOption('input');
+
+        if ($metricsFile && \is_file($metricsFile)) {
+            $this->dataAnalyzer->loadFileData($metricsFile);
+            $result = $this->dataAnalyzer->processData();
+
+            $output->write($this->textFormatter->output($result));
+        }
+        else {
+            throw new \LogicException('Missing data');
+        }
+
     }
 }
